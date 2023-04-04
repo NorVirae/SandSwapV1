086b56b3db61902965fd4dde4e6edd26829f7709
Norbert Frank Mba <norbertmbafrank@gmail.com>
2023-04-04 14:45:18 +0100

    make token address public

--- contracts/Exchange.sol
+++ contracts/Exchange.sol
@@ -1,7 +1,7 @@
 pragma solidity ^0.8.0;
 
 contract Exchange{
-    address tokenAddress;
+    address public tokenAddress;
 
     constructor(address _token){
         require(_token != address(0), "invalid token address");
